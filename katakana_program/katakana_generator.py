49db851083a7020d1b2d0426d657e81a30d6e088
Shien Fu <ShienFu99@gmail.com>
2023-10-02 16:59:13 -0400

    Updated katakana_generator.py + added a README.md file to support it

--- katakana_program/katakana_generator.py
+++ katakana_program/katakana_generator.py
@@ -1,147 +1,169 @@
-"""
-    katakana_generator.py
-    Author: ShienFu99
-    Date: 8/23/2023
-    Description: A program that will lets the user practice reading katakana
-    > Generates a random string of katakana (without repeating characters)
-    > Expects the user to input the translation in romaji
-    > The user gets 3 lives before the program shows the correct translation
-"""
-
-#Imports
-import random
-from maskpass import askpass
-import os
+#Built-in Imports
+from getpass import getpass
+from os import system
+from random import choice
+from sys import exit
 
 
 def main():
+    #Hide command that runs the program
     clear_console()
 
+    #User starts with 3 lives
     user_lives = 3
 
+    #Maps each katakana character to its roumaji equivalent
     katakana = [
-        {"katakana": "ア", "romaji": "a"},
-        {"katakana": "イ", "romaji": "i"},
-        {"katakana": "ウ", "romaji": "u"},
-        {"katakana": "エ", "romaji": "e"},
-        {"katakana": "オ", "romaji": "o"},
-        {"katakana": "カ", "romaji": "ka"},
-        {"katakana": "キ", "romaji": "ki"},
-        {"katakana": "ク", "romaji": "ku"},
-        {"katakana": "ケ", "romaji": "ke"},
-        {"katakana": "コ", "romaji": "ko"},
-        {"katakana": "サ", "romaji": "sa"},
-        {"katakana": "シ", "romaji": "shi"},
-        {"katakana": "ス", "romaji": "su"},
-        {"katakana": "セ", "romaji": "se"},
-        {"katakana": "ソ", "romaji": "so"},
-        {"katakana": "タ", "romaji": "ta"},
-        {"katakana": "チ", "romaji": "chi"},
-        {"katakana": "ツ", "romaji": "tsu"},
-        {"katakana": "テ", "romaji": "te"},
-        {"katakana": "ト", "romaji": "to"},
-        {"katakana": "ナ", "romaji": "na"},
-        {"katakana": "ニ", "romaji": "ni"},
-        {"katakana": "ヌ", "romaji": "nu"},
-        {"katakana": "ネ", "romaji": "ne"},
-        {"katakana": "ノ", "romaji": "no"},
-        {"katakana": "ハ", "romaji": "ha"},
-        {"katakana": "ヒ", "romaji": "hi"},
-        {"katakana": "フ", "romaji": "fu"},
-        {"katakana": "ヘ", "romaji": "he"},
-        {"katakana": "ホ", "romaji": "ho"},
-        {"katakana": "マ", "romaji": "ma"},
-        {"katakana": "ミ", "romaji": "mi"},
-        {"katakana": "ム", "romaji": "mu"},
-        {"katakana": "メ", "romaji": "me"},
-        {"katakana": "モ", "romaji": "mo"},
-        {"katakana": "ヤ", "romaji": "ya"},
-        {"katakana": "ユ", "romaji": "yu"},
-        {"katakana": "ヨ", "romaji": "yo"},
-        {"katakana": "ラ", "romaji": "ra"},
-        {"katakana": "リ", "romaji": "ri"},
-        {"katakana": "ル", "romaji": "ru"},
-        {"katakana": "レ", "romaji": "re"},
-        {"katakana": "ロ", "romaji": "ro"},
-        {"katakana": "ワ", "romaji": "wa"},
-        {"katakana": "ヲ", "romaji": "wo"},
-        {"katakana": "ン", "romaji": "n"}
+        {"katakana": "ア", "roumaji": "a"},
+        {"katakana": "イ", "roumaji": "i"},
+        {"katakana": "ウ", "roumaji": "u"},
+        {"katakana": "エ", "roumaji": "e"},
+        {"katakana": "オ", "roumaji": "o"},
+        {"katakana": "カ", "roumaji": "ka"},
+        {"katakana": "キ", "roumaji": "ki"},
+        {"katakana": "ク", "roumaji": "ku"},
+        {"katakana": "ケ", "roumaji": "ke"},
+        {"katakana": "コ", "roumaji": "ko"},
+        {"katakana": "サ", "roumaji": "sa"},
+        {"katakana": "シ", "roumaji": "shi"},
+        {"katakana": "ス", "roumaji": "su"},
+        {"katakana": "セ", "roumaji": "se"},
+        {"katakana": "ソ", "roumaji": "so"},
+        {"katakana": "タ", "roumaji": "ta"},
+        {"katakana": "チ", "roumaji": "chi"},
+        {"katakana": "ツ", "roumaji": "tsu"},
+        {"katakana": "テ", "roumaji": "te"},
+        {"katakana": "ト", "roumaji": "to"},
+        {"katakana": "ナ", "roumaji": "na"},
+        {"katakana": "ニ", "roumaji": "ni"},
+        {"katakana": "ヌ", "roumaji": "nu"},
+        {"katakana": "ネ", "roumaji": "ne"},
+        {"katakana": "ノ", "roumaji": "no"},
+        {"katakana": "ハ", "roumaji": "ha"},
+        {"katakana": "ヒ", "roumaji": "hi"},
+        {"katakana": "フ", "roumaji": "fu"},
+        {"katakana": "ヘ", "roumaji": "he"},
+        {"katakana": "ホ", "roumaji": "ho"},
+        {"katakana": "マ", "roumaji": "ma"},
+        {"katakana": "ミ", "roumaji": "mi"},
+        {"katakana": "ム", "roumaji": "mu"},
+        {"katakana": "メ", "roumaji": "me"},
+        {"katakana": "モ", "roumaji": "mo"},
+        {"katakana": "ヤ", "roumaji": "ya"},
+        {"katakana": "ユ", "roumaji": "yu"},
+        {"katakana": "ヨ", "roumaji": "yo"},
+        {"katakana": "ラ", "roumaji": "ra"},
+        {"katakana": "リ", "roumaji": "ri"},
+        {"katakana": "ル", "roumaji": "ru"},
+        {"katakana": "レ", "roumaji": "re"},
+        {"katakana": "ロ", "roumaji": "ro"},
+        {"katakana": "ワ", "roumaji": "wa"},
+        {"katakana": "ヲ", "roumaji": "wo"},
+        {"katakana": "ン", "roumaji": "n"}
     ]
 
-    user_level = get_user_level()
+    #Gets the difficulty level from the user
+    difficulty_level = get_difficulty_level()
 
-    random_str, random_str_translation = generate_random_str(katakana, user_level)
+    #Generates a random string of N (unique) katakana characters -> N is the difficulty_level
+    #Also generates the roumaji equivalent
+    katakana_str, katakana_str_translation = generate_katakana_str(katakana, difficulty_level)
 
+    #Displays remaining lives
     print(f"\nLives remaining: {user_lives}\n")
 
-    print(random_str)
+    #Displays randomly generated katakana string
+    print(katakana_str)
 
-    user_translate(user_lives, random_str, random_str_translation)
+    #Gets the user's translation and evaluates it -> If the user runs out of lives, returns to main
+    user_translate(user_lives, katakana_str, katakana_str_translation)
 
-    proceed()
-
+    proceed("Press Enter to exit the program...")
     clear_console()
 
 
-def get_user_level():
+def get_difficulty_level():
+    #Prompts user to input a difficulty level between 1 and 46 (inclusive) -> Reprompts until input is valid, then returns it to main
     while True:
         try:
-            user_level = int(input("Set level (between 1 and 46): "))
-            if user_level > 46 or user_level < 1:
+            difficulty_level = int(input("Set level (between 1 and 46): "))
+            if difficulty_level > 46 or difficulty_level < 1:
                 raise ValueError
-            return user_level
+            return difficulty_level
         except ValueError:
             clear_console()
             print("Invalid level! Try again.")
 
 
-def generate_random_str(katakana, user_level):
-    random_str = ""
-    random_str_translation = ""
+def generate_katakana_str(katakana, difficulty_level):
+    katakana_str = ""
+    katakana_str_translation = ""
 
-    while user_level != 0:
-        user_level -= 1
-        katakana_char = random.choice(katakana)
-        random_str += katakana_char["katakana"]
-        random_str_translation += katakana_char["romaji"]
+    #Runs loop N times -> N = difficulty level
+    while difficulty_level != 0:
+        difficulty_level -= 1
+        #Selects a random katakana character
+        katakana_char = choice(katakana)
+        #Adds the randomly selected katakana character to the katakana_str + its roumaji equivalent to the translated string
+        katakana_str += katakana_char["katakana"]
+        katakana_str_translation += katakana_char["roumaji"]
+        #Removes the selected katakana character from the list to prevent repeats
         katakana.remove(katakana_char)
 
-    return (random_str, random_str_translation)
+    #Returns the hiragan string and its translation to main()
+    return (katakana_str, katakana_str_translation)
 
 
-def user_translate(user_lives, random_str, random_str_translation):
+def user_translate(user_lives, katakana_str, katakana_str_translation):
+    user_guesses = []
+    guess_count = 0
 
-    print("What is the translation in romaji?\n")
+    print("What is the translation in roumaji?\n")
 
+    #Evalutates user's translation and responds appropriately
     while True:
-        user_romaji = input()
-        if user_romaji == random_str_translation:
-            clear_console()
+        #Each guess is saved in a list
+        user_guesses.append(input())
+        clear_console()
+
+        #If the user's translation is correct, let them know and exit the loop -> ends the program
+        if user_guesses[guess_count] == katakana_str_translation:
             print("Correct!\n")
+            #Exits while-loop, returning to main()
             break
+
+        #If the user's translation is wrong...
         else:
+            #Decrease lives by 1, guess counter by 1
             user_lives -= 1
+            guess_count += 1
+
+            #If the user guesses wrong 3 times in a row, show the proper translation and display their previous guesses -> break out of the loop and end the progam
             if user_lives == 0:
-                clear_console()
-                print(f"{random_str}\n")
-                print(f"Incorrect! The translation is \"{random_str_translation}\".")
+                print(f"{katakana_str}\n")
+                print(f"Incorrect! The translation is \"{katakana_str_translation}\".")
+                print("\nYour previous guesses:")
+                for num, guess in enumerate(user_guesses):
+                    print(f"Guess {num+1}: {guess}")
+                print()
                 break
+
+            #If the user still has remaining lives, let them know their guess is incorrect and display the updated lives counter -> Reprompt for a new guess
             else:
-                clear_console()
                 print("Incorrect! Try again.")
                 print(f"Lives remaining: {user_lives}\n")
-                print(random_str)
-                print("\nWhat is the translation in romaji?\n")
-                continue
+                print(katakana_str)
+                print("\nWhat is the translation in roumaji?\n")
 
 
-def proceed():
-    choice = askpass("Press enter to continue...", mask="")
+def proceed(prompt):
+    #Hides user input + doesn't store it -> Program pauses until the user presses Enter
+    getpass(prompt)
 
 
 def clear_console():
     #Clears the terminal when run
-    os.system("clear")
+    system("clear")
 
 
 if __name__ == "__main__":
